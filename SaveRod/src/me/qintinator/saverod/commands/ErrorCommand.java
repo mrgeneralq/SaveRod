51ae98cbfed1da9762595cde6ea5ccfed44ffd2c
MrGeneralQ <mrgeneralq@gmail.com>
2018-09-13 12:23:15 +0200

    Changed API color

--- SaveRod/src/me/qintinator/saverod/commands/ErrorCommand.java
+++ SaveRod/src/me/qintinator/saverod/commands/ErrorCommand.java
@@ -1,10 +1,10 @@
 package me.qintinator.saverod.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 import me.qintinator.saverod.contracts.ISaveRodSubCommand;
-import net.md_5.bungee.api.ChatColor;
 
 public class ErrorCommand implements ISaveRodSubCommand {
 
